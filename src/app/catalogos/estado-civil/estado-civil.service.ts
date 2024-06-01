6586aa5ffe5f2255a2b34e62ddb1dbe44263f6c3
camperitox <ml17017@ues.edu.sv>
2024-06-01 16:01:05 -0600

    ✨ feat: actualizar modelo y servicio de EstadoCivil

--- src/app/catalogos/estado-civil/estado-civil.service.ts
+++ src/app/catalogos/estado-civil/estado-civil.service.ts
@@ -2,40 +2,41 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { EstadoCivil } from "../../core/models/EstadoCivil";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 
 @Injectable({
     providedIn: 'root',
 })
 export class estadoCivilService{
-    private url: string = ""; //Aqui va el url de la api
+    private url: string = environment.apiUrl;
 
     constructor(private http: HttpClient){}
 
     //Obtiene la lista de estado civil
     getAll(): Observable<EstadoCivil[]>{
-        return this.http.get<EstadoCivil[]>(this.url + '/estado-civil/');
+        return this.http.get<EstadoCivil[]>(this.url + '/estados_civiles/');
     }
 
     //método que permite crear un nuevo estado civil
     create(estadocivil:EstadoCivil): Observable<EstadoCivil>{
-        return this.http.put<EstadoCivil>(this.url + '/estado-civil/', estadocivil)
+        return this.http.post<EstadoCivil>(this.url + '/estados_civiles/', estadocivil)
     };
 
     //método que obtiene un solo estado civil
     get(id: number): Observable<EstadoCivil>{
-        return this.http.get<EstadoCivil>(this.url + '/estado-civil/' + id);
+        return this.http.get<EstadoCivil>(this.url + '/estados_civiles/' + id);
     }
 
     //método para actualizar estado civil
     update(estadocivil: EstadoCivil): Observable<EstadoCivil>{
         return this.http.put<EstadoCivil>(
-            this.url + '/estado-civil/' + estadocivil.codigo_estado_civil + '/', estadocivil
+            this.url + '/estados_civiles/' + estadocivil.id_estado_civil + '/', estadocivil
         );
     }
 
     //método para eliminar estado civil
     delete(id?:number): Observable<EstadoCivil>{
-        return this.http.delete<EstadoCivil>(this.url + '/estado-civil/' + id);
+        return this.http.delete<EstadoCivil>(this.url + '/estados_civiles/' + id);
     }
 }
