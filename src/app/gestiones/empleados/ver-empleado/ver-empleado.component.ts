1764cfb1c58563eaea760e254a865eb1b8e5ff82
Belserion21 <rasa0821@hotmail.com>
2024-06-09 15:39:06 -0600

    version ron2

--- src/app/gestiones/empleados/ver-empleado/ver-empleado.component.ts
+++ src/app/gestiones/empleados/ver-empleado/ver-empleado.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from "../../../shared/Components/header/header.component";
-
+import { Empleado } from '../../../core/models/Empleados';
+import { empleadoService } from '../empleado.service';
 
 @Component({
-    selector: 'app-ver-empleado',
-    standalone: true,
-    templateUrl: './ver-empleado.component.html',
-    styleUrl: './ver-empleado.component.css',
-    imports: [HeaderComponent]
+  selector: 'app-ver-empleado',
+  standalone: true,
+  templateUrl: './ver-empleado.component.html',
+  styleUrls: ['./ver-empleado.component.css'],
+  imports: [HeaderComponent,CommonModule]
 })
-export class VerEmpleadoComponent {
-   
+export class VerEmpleadoComponent implements OnInit {
+  empleado?: Empleado;
+
+  constructor(
+    private route: ActivatedRoute,
+    private empleadoService: empleadoService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.empleadoService.get(+id).subscribe((empleado) => (this.empleado = empleado));
+    }
+  }
 }
