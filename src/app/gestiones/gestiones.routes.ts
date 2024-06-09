eafb4d5fa35128dee7fe9c9ff57de5e7f42f4ddc
Nancy <nancylissethramirezservellon@gmail.com>
2024-06-08 18:18:38 -0600

    boletas de pago y planilla de pago pdf

--- src/app/gestiones/gestiones.routes.ts
+++ src/app/gestiones/gestiones.routes.ts
@@ -21,11 +21,11 @@ export const GESTIONES_ROUTES: Routes = [
     {path: 'usuario', component: UsuarioComponent},
 
     {path: 'empleados/registrar-empleado', component:RegistrarEmpleadoComponent},
-    {path: 'empleados/ver-empleado/{id}', component:VerEmpleadoComponent},
+    {path: 'empleados/ver-empleado/:id', component:VerEmpleadoComponent},
 
     {path: 'planilla/generar-planilla', component:GenerarPlanillaComponent},
-    {path: 'planilla/editar-planilla/{id}', component:EditarPlanillaComponent},
-    {path: 'planilla/boleta-pago/{id}', component:BoletaPagoEmpleadoComponent},
+    {path: 'planilla/editar-planilla/:id', component:EditarPlanillaComponent},
+    {path: 'planilla/boleta-pago/:id', component:BoletaPagoEmpleadoComponent},
 
 
 ];
--- src/app/gestiones/planilla/boleta-pago-empleado/boleta-pago-empleado.component.ts
+++ src/app/gestiones/planilla/boleta-pago-empleado/boleta-pago-empleado.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../shared/Components/header/header.component";
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-boleta-pago-empleado',
     standalone: true,
     templateUrl: './boleta-pago-empleado.component.html',
-    styleUrl: './boleta-pago-empleado.component.css',
-    imports: [HeaderComponent]
+    styleUrls: ['./boleta-pago-empleado.component.css'],
+    imports: [HeaderComponent, CommonModule]
 })
 export class BoletaPagoEmpleadoComponent {
+    generatePDF() {
+        const elementToPrint = document.getElementById("view");
+        const fileNameElement = document.getElementById("file-name");
+        const fileName = fileNameElement ? fileNameElement.innerText.trim() : 'default';
 
+        if (elementToPrint) {
+            console.log("Element found, generating PDF...");
+
+            html2canvas(elementToPrint, { scale: 2 }).then((canvas) => {
+                const pdf = new jsPDF('l', 'mm', 'letter');
+                const imgData = canvas.toDataURL('image/png');
+
+                const imgProps = pdf.getImageProperties(imgData);
+                const pdfWidth = pdf.internal.pageSize.getWidth();
+                const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+                // Margen superior ajustado
+                const marginTop = 20; 
+
+                pdf.addImage(imgData, 'PNG', 0, marginTop, pdfWidth, pdfHeight);
+
+                pdf.setProperties({
+                    title: `Boleta de pago de ${fileName}`,
+                    subject: "",
+                    author: "Contasync"
+                });
+
+                pdf.setFontSize(12);
+                pdf.text('', 14, marginTop + 22); // Ajusta la posición del texto adicional
+
+                pdf.save(`${fileName}.pdf`);
+
+                console.log("PDF generated successfully.");
+            }).catch((error) => {
+                console.error("Error generating PDF: ", error);
+            });
+        } else {
+            console.error("Element with id 'view' not found.");
+        }
+    }
 }
